3740dfb618ac9641098bcb04178fc5353654a2cb
Nick <nickmagi24@gmail.com>
2022-03-16 09:43:11 -0400

    Basic API data display

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,12 +1,11 @@
-import create from '@ant-design/icons/lib/components/IconFont';
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const cryptoApiHeader = {
+const cryptoApiHeaders = {
   'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
   'x-rapidapi-key': 'f695ad86d3msh2382093387e1a84p1d4f1ajsn4b3af5d02410'
 }
 
-const baseUrl = 'https://coinranking1.p.rapidapi.com/coins';
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
 
@@ -15,7 +14,11 @@ export const cryptoApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
-      query: () => createRequest('/exchanges')
+      query: () => createRequest('/coins')
     })
   })
 });
+
+export const { 
+  useGetCryptosQuery, 
+} = cryptoApi;
