ab993b39fd0fb431f9778f2e50e47f2651fe2c62
Nick Magidson <Nickmagi24@gmail.com>
2022-03-22 10:09:16 -0400

    News component rendering

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -7,6 +7,8 @@ import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 const { Text, Title } = Typography;
 const { Option } = Select;
 
+const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
+
 const News = ({ simplified }) => {
   const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12 });
 
@@ -20,7 +22,11 @@ const News = ({ simplified }) => {
             <a href={news.url} target='_blank' rel='noreferrer'>
               <div className='news-image-container'>
                 <Title className='news-title' level={4}>{news.name}</Title>
+                <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt='news' />
               </div>
+              <p>
+                {news.description > 100 ? `${news.description.subString(0, 100)} ...` : news.description}
+              </p>
             </a>
           </Card>
         </Col>
