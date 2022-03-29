2ad89e82ca2a1931ce8227dc6287bc66d7862a23
Nick Magidson <Nickmagi24@gmail.com>
2022-03-29 09:21:17 -0400

    cryptoDetails rendering

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -20,21 +20,22 @@ const CryptoDetails = () => {
 
   if (isFetching) return 'Loading...';
 
-  // const stats = [
-  //   { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
-  //   { title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
-  //   { title: '24h Volume', value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`, icon: <ThunderboltOutlined /> },
-  //   { title: 'Market Cap', value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`, icon: <DollarCircleOutlined /> },
-  //   { title: 'All-time-high(daily avg.)', value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`, icon: <TrophyOutlined /> },
-  // ];
+  const stats = [
+    { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(Number(cryptoDetails.price))}`, icon: <DollarCircleOutlined /> },
+    { title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
+    { title: '24h Volume', value: `$ ${cryptoDetails["24hVolume"] && millify(Number(cryptoDetails["24hVolume"]))}`, icon: <ThunderboltOutlined /> },
+    { title: 'Market Cap', value: `$ ${cryptoDetails.marketCap && millify(Number(cryptoDetails.marketCap))}`, icon: <DollarCircleOutlined /> },
+    { title: 'All-time-high(daily avg.)', value: `$ ${millify(Number(cryptoDetails.allTimeHigh.price))}`, icon: <TrophyOutlined /> },
+  ];
 
-  // const genericStats = [
-  //   { title: 'Number Of Markets', value: cryptoDetails.numberOfMarkets, icon: <FundOutlined /> },
-  //   { title: 'Number Of Exchanges', value: cryptoDetails.numberOfExchanges, icon: <MoneyCollectOutlined /> },
-  //   { title: 'Aprroved Supply', value: cryptoDetails.approvedSupply ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
-  //   { title: 'Total Supply', value: `$ ${millify(cryptoDetails.totalSupply)}`, icon: <ExclamationCircleOutlined /> },
-  //   { title: 'Circulating Supply', value: `$ ${millify(cryptoDetails.circulatingSupply)}`, icon: <ExclamationCircleOutlined /> },
-  // ];
+  const genericStats = [
+    { title: 'Number Of Markets', value: cryptoDetails.numberOfMarkets, icon: <FundOutlined /> },
+    { title: 'Number Of Exchanges', value: cryptoDetails.numberOfExchanges, icon: <MoneyCollectOutlined /> },
+    { title: 'Aprroved Supply', value: cryptoDetails.change ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
+    { title: 'Total Supply', value: `$ ${millify(Number(cryptoDetails?.supply?.total))}`, icon: <ExclamationCircleOutlined /> },
+    { title: 'Circulating Supply', value: `$ ${millify(Number(cryptoDetails?.supply?.circulating))}`, icon: <ExclamationCircleOutlined /> },
+  ];
+
 
   return (
     <Col className='coin-detail-container'>
@@ -42,6 +43,59 @@ const CryptoDetails = () => {
         <Title level={2} className='coin-name'>
           {cryptoDetails.name} ({cryptoDetails.symbol}) Price
         </Title>
+        <p>
+          {cryptoDetails.name} live price in US dollars.
+          View value statistics, market cap and supply.
+        </p>
+      </Col>
+      <Select 
+        defaultValue='7d'
+        className='select-timeperiod' 
+        placeholder='Select Time Period'
+        onChange={(value) => setTimePeriod(value)}
+        >
+          {time.map((date) => <Option key={date}>{date}</Option>)}
+      </Select>
+      {/* Line chart */}
+      <Col className='stats-container'>
+        <Col className='coin-value-statistics'>
+          <Col className='coin-value=statistics-heading'>
+            <Title level={3} className='coin-details-heading'>
+              {cryptoDetails.name} Value Statistics
+            </Title>
+            <p>
+              An overview showing the stats of {cryptoDetails.name}
+            </p>
+          </Col>
+          {stats.map(({ icon, title, value }) => (
+            <Col className='coin-stats'>
+              <Col className='coin-stats-name'>
+                <Text>{icon}</Text>
+                <Text>{title}</Text>
+              </Col>
+              <Text className='stats'>{value}</Text>
+            </Col>
+          ))}
+        </Col>
+        <Col className='other-stats-info'>
+          <Col className='coin-value=statistics-heading'>
+            <Title level={3} className='coin-details-heading'>
+              {cryptoDetails.name} Value Statistics
+            </Title>
+            <p>
+              An overview showing the stats of {cryptoDetails.name}
+            </p>
+          </Col>
+          {genericStats.map(({ icon, title, value }) => (
+            <Col className='coin-stats'>
+              <Col className='coin-stats-name'>
+                <Text>{icon}</Text>
+                <Text>{title}</Text>
+              </Col>
+              <Text className='stats'>{value}</Text>
+            </Col>
+          ))}
+        </Col>
       </Col>
     </Col>
   )
