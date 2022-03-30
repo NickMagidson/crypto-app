9ab5b658b549a2cb05e07bebea3d98e5e7945014
Nick <nickmagi24@gmail.com>
2022-03-30 08:42:52 -0400

    Temp

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,7 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+  'x-bingapis-sdk': 'true',
+  'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+  'x-rapidapi-key': 'f695ad86d3msh2382093387e1a84p1d4f1ajsn4b3af5d02410'
+}
