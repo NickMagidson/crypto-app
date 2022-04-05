2405921ba4e8fc3cc24bf441e0ded7d0b30b4f23
Nick Magidson <Nickmagi24@gmail.com>
2022-04-05 19:36:52 -0400

    Loader and demo Exchanges

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -21,12 +21,16 @@ export const cryptoApi = createApi({
     }),
     getCryptoHistory: builder.query({
       query: ({ coinId, timePeriod }) => createRequest(`coin/${coinId}/history?timePeriod=${timePeriod}`),
-  })
+    }),
+    getExchanges: builder.query({
+      query: () => createRequest('/exchanges'),
+    })
   })
 });
 
 export const { 
   useGetCryptosQuery, 
 	useGetCryptoDetailsQuery,
-  useGetCryptoHistoryQuery
+  useGetCryptoHistoryQuery,
+  useGetExchangesQuery
 } = cryptoApi;
