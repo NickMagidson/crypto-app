1b3e56f5ca6af9cb452c9595c37d36c9952dc351
Nick Magidson <Nickmagi24@gmail.com>
2023-02-21 13:17:33 -0500

    Implimented loader message due to bing new API error

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,6 +4,8 @@ import { Spin } from 'antd';
 const Loader = () => {
     return(
         <div className="loader">
+        <h1>We're experiencing errors with out news API.</h1>
+        <h3>Please check back later :)</h3>
             <Spin />
         </div>
     )
